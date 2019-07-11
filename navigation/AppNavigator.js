733f402caa721c717ffd53bc7f3c6174713b39a6
Brendo <brendo.morassi@gtc.ag>
2019-07-11 01:42:52 -0300

    init project

---
+++ navigation/AppNavigator.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {
+  ActivityIndicator,
+  AsyncStorage,
+  Button,
+  StatusBar,
+  StyleSheet,
+  View,
+} from 'react-native';
+import { createStackNavigator, createSwitchNavigator, createAppContainer } from 'react-navigation';
+
+
+import HomeScreen from '../screens/HomeScreen';
+import OtherScreen from '../screens/OtherScreen';
+import SignInScreen from '../screens/auth/SignInScreen';
+import AuthLoadingScreen from '../screens/auth/AuthLoadingScreen';
+
+
+// import MainTabNavigator from './MainTabNavigator';
+
+// export default createAppContainer(
+//   createSwitchNavigator({
+//     // You could add another route here for authentication.
+//     // Read more at https://reactnavigation.org/docs/en/auth-flow.html
+//     Main: MainTabNavigator,
+//   })
+// );
+
+
+const AppStack = createStackNavigator({ Home: HomeScreen, Other: OtherScreen });
+const AuthStack = createStackNavigator({ SignIn: SignInScreen });
+
+export default createAppContainer(createSwitchNavigator(
+  {
+    AuthLoading: AuthLoadingScreen,
+    App: AppStack,
+    Auth: AuthStack,
+  },
+  {
+    initialRouteName: 'AuthLoading',
+  }
+));
